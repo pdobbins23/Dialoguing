89bfc587401809d4077186897559ad706f6e94fc
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-02-28 15:42:20 -0800

    Finished

--- src/scenes/Talking.js
+++ src/scenes/Talking.js
@@ -7,7 +7,7 @@ class Talking extends Phaser.Scene {
         // dialog constants
         this.DBOX_X = 0			        // dialog box x-position
         this.DBOX_Y = 400			    // dialog box y-position
-        this.DBOX_FONT = 'gem_font'	    // dialog box font key
+        this.DBOX_FONT = 'minecraft_font'	    // dialog box font key
 
         this.TEXT_X = 50			    // text w/in dialog box x-position
         this.TEXT_Y = 445			    // text w/in dialog box y-position
@@ -53,6 +53,7 @@ class Talking extends Phaser.Scene {
         this.minerva = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'minerva').setOrigin(0, 1)
         this.neptune = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'neptune').setOrigin(0, 1)
         this.jove = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'jove').setOrigin(0, 1)
+        this.mickey = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'mickey').setOrigin(0, 1)
 
         // input
         cursors = this.input.keyboard.createCursorKeys()
